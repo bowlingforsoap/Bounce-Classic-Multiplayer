6db98da894ccdd680dfc111c3b562d0e0b5bd9ff
Holo-Vadym <v.strelchenko@holo-light.com>
2024-06-25 17:33:32 +0200

    [WIP] started adding buttons to connect Network Player and spawn Player Prefabs for them

--- Assets/Scripts/GuiController.cs
+++ Assets/Scripts/GuiController.cs
@@ -1,10 +1,22 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class GuiController : MonoBehaviour
 {
+    [SerializeField] private UIDocument _networkOverlay;
     [SerializeField] private UIDocument _gameOverUi;
 
+    private void Start()
+    {
+        ShowNetworkOverlay();
+    }
+
+    public void ShowNetworkOverlay()
+    {
+        SetGuiEnabled(_networkOverlay, true);
+    }
+    
     public void ShowGameOverScreen()
     {
         SetGuiEnabled(_gameOverUi, true);
